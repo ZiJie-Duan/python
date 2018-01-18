2951511083d30ca51df56abbe4f1faa9a41888fe
lucycore <32613263+lucycore@users.noreply.github.com>
2018-01-19 00:45:12 +0800

    Add files via upload

---
+++ python/python_work4/if5dyd.py
@@ -0,0 +1,19 @@
+a = 17
+a_1 = 16
+
+if (a < 18) and (a_1 < 18):
+	print("sorry!")
+else:
+	print("go!")
+	
+print("-------------------------------------")
+
+b = "hello world!"
+
+if b == "hello world":
+	print("true")
+else:
+	print("false")
+	
+print("-------------------------------------")
+
