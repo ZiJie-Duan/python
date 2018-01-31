183bc616bbc9a0ec9ef5c161a1c84ed6680e572b
lucycore <1296720721@qq.com>
2018-01-31 17:29:13 +0800

    函数

--- python/python_work7/def7.py
+++ python/python_work7/def7.py
@@ -2,4 +2,21 @@
 
 names = ['dzj','ljx','wwb','gyj','zke']
 
-def name()
+new_names = []
+
+ls_names = names[:]
+
+#while hi 意思是使用pop()函数删除列表中的值
+#并且判断hi列表中是否有剩余的值
+
+def name(hi):
+	while hi:   
+		new_name = 'wow ' + hi.pop()  
+		print(new_name)
+		new_names.append(new_name)
+
+name(ls_names)
+
+print(new_names)
+
+print(names)
