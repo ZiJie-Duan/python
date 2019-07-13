d5c6f7844e798068e3a20d159a2556d690d46615
lucycore <1296720721@qq.com>
2019-07-13 21:45:37 +0800

    .

---
+++ python/play/练习/欧拉/三位数相乘最大回文.py
@@ -0,0 +1,24 @@
+
+def make_time_list():
+	re = []
+	for x in range(100,1000):
+		for y in range(100,1000):
+			c = x * y
+			c = str(c)
+			re.append(c)
+
+	return re
+
+def get_hw(l):
+
+	for x in l:
+		a = list(x)
+		zs = len(a)
+		if zs%2 == 0:
+			for 
+
+
+		else:
+
+
+
